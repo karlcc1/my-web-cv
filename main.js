eb8bc6c941bb140ab2952c36bf3ee90ab6f6d212
Karl C <karl.cc14@gmail.com>
2020-09-09 20:41:31 +1200

    sidenav transition

---
+++ main.js
@@ -0,0 +1,6 @@
+function hamburgclick() {
+    document.getElementById("sidenav").classList.toggle("active");
+    document.getElementById("main-body").classList.toggle("active");
+
+    // document.getElementById("fullname").style.color = "black";
+}
